4bf97e80659fb0a1e5dfa0c0aea52d69577554f5
MarkoDavkovski <marko.davkovski@gmail.com>
2023-06-12 19:35:12 +0200

    Possible moves function created.

--- main.js
+++ main.js
@@ -9,7 +9,27 @@ const CreateBoard = (() => {
 	}
 })();
 
-const possibleMoves = ([x, y]) => {};
+const possibleMoves = ([x, y]) => {
+	const moves = [
+		[1, 2],
+		[1, -2],
+		[-1, 2],
+		[-1, -2],
+		[2, 1],
+		[2, -1],
+		[-2, 1],
+		[-2, -1],
+	];
+	const validMoves = [];
+	for (let move of moves) {
+		const newX = x + move[0];
+		const newY = y + move[1];
+		if (newX >= 0 && newX <= 7 && newY >= 0 && newY <= 7) {
+			validMoves.push([newX, newY]);
+		}
+		return validMoves;
+	}
+};
 
 const findShortestPath = (start, end) => {};
 
